3c141459559c182d0743571a4d54966184f17b69
luvpretty <975551732@qq.com>
2019-11-27 21:53:41 +0800

    新增mongodb,redis数据库配置

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+const DB_URL = 'mongodb://test:123456@47.105.212.161:15000/testdb'
+const REDIS = {
+  host: '47.105.212.161',
+  port: 15001,
+  password: '123456'
+}
+const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
+
+export default {
+  DB_URL,
+  REDIS,
+  JWT_SECRET
+}
